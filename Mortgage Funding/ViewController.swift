59fe4a9ee12c559b56b41d6c33e2b0f8c05c59e3
Abdul Ahad <saimali@umers-mbp.home>
2019-04-22 14:50:09 +0500

    Initial Commit

---
+++ Mortgage Funding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mortgage Funding
+//
+//  Created by Saim Ali on 22/04/2019.
+//  Copyright © 2019 Saim Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
